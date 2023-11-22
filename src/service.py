0240c1932f5f37e6c01baee5651d35dfd2dea2c6
Salman-18 <salmannizamani205@gmail.com>
2023-11-22 18:18:24 +0500

    Mocking

---
+++ src/service.py
@@ -0,0 +1,18 @@
+import requests
+database = {
+    1: "salman",
+    2 : "ahmed",
+    3 : "anshal",
+    4 : "ishaque"
+}
+
+def get_user_from_db(user_id):
+    return database.get(user_id)
+
+def get_users():
+    response = requests.get("https://jsonplaceholder.typicode.com/")
+    if response.status_code  == 200:
+        return response.json()
+    raise requests.HTTPError
+
+
