fc00369a57768b1b6c540126d10dd43eb9b136bb
Salman-18 <salmannizamani205@gmail.com>
2023-11-22 14:56:22 +0500

    mark and parametrize

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -4,20 +4,21 @@ import math
 class TestCircle:
     def setup_method(self, method):
         print(f"setting up {method}")
-        self.cicrle = shapes.Circle(10)
+        self.circle = shapes.Circle(10)
     
     def teardown_method(self, method):
         print(f"teardown down {method}")
+        del self.circle
 
     def test_area(self):
-        assert self.cicrle.area == math.pi * self.cicrle.radius ** 2
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
     
     def test_perimeter(self):
-        result = self.cicrle.perimeter()
-        expected = 2 * math.pi * self.cicrle.perimeter
+        result = self.circle.perimeter()
+        expected = 2 * math.pi * self.circle.radius
         assert result == expected
 
     def test_not_same_area_rectangle(self, my_rectangle):
-        assert self.cicrle.area() != my_rectangle.area()
+        assert self.circle.area() != my_rectangle.area()
 
 
