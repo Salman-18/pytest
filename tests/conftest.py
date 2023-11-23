c4e25c7dc889cfe25750335cedeabe12ea0db672
Salman-18 <salmannizamani205@gmail.com>
2023-11-23 15:20:14 +0500

    mangement of school

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import src.shapes as shapes
+from src.school import ClassRoom, Teacher, Student
 @pytest.fixture
 def my_rectangle():
     return shapes.Rectangle(10, 20)
@@ -7,3 +8,10 @@ def my_rectangle():
 @pytest.fixture
 def your_rectangle():
     return shapes.Rectangle(5, 6)
+
+@pytest.fixture
+def sample_classroom():
+    teacher = Teacher(name="Professor Dumbledore")
+    students = [Student(name=f"Student {i+1}") for i in range(10)]
+    course_title = "Defense Against the Dark Arts"
+    return ClassRoom(teacher, students, course_title)
