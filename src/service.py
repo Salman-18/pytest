1e580a237ea7b4dd7f15341d0f524b28b3f89108
Salman-18 <salmannizamani205@gmail.com>
2023-11-25 17:13:37 +0500

    correct url

--- src/service.py
+++ src/service.py
@@ -10,9 +10,10 @@ def get_user_from_db(user_id):
     return database.get(user_id)
 
 def get_users():
-    response = requests.get("https://jsonplaceholder.typicode.com/")
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
     if response.status_code  == 200:
         return response.json()
-    raise requests.HTTPError
+    raise requests.HTTPError("Failed to retrieve data from the API. Status code: {}".format(response.status_code))
+
 
 
