8e980fdf25a1b9723225128079419dea616b1496
Salman Nizamani <80026274+Salman-18@users.noreply.github.com>
2023-12-19 13:08:21 +0500

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,7 +1,7 @@
 import pytest
 import src.shapes as shapes 
 
-@pytest.mark.parametrize("side_length, expected_area", [(5, 44), (4, 16), (9, 81)])
+@pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81)])
 def test_multiple_square_areas(side_length, expected_area):
     assert shapes.Square(side_length).area() == expected_area
 
